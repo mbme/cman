552d8262bbeef71b566df790c92beced1c58f58f
mbme <stribog.ua@gmail.com>
2014-05-23 19:50:47 +0300

    test improvements

--- spec/executor_spec.rb
+++ spec/executor_spec.rb
@@ -6,61 +6,71 @@ describe Cman::Executor do
     FileUtils.mkdir_p BASE_DIR
   end
 
-  def new(command)
-    Cman::Executor.new command
+  def new(command, mock)
+    comm = Cman::Executor.new command
+    comm.should_receive(:gets).and_return('y') if mock
+    comm
+  end
+
+  def new_add(mock: true)
+    new 'add', mock
+  end
+
+  def new_remove(mock: true)
+    new 'remove', mock
+  end
+
+  def new_stats
+    new 'stats', false
+  end
+
+  def new_install(mock: true)
+    new 'install', mock
+  end
+
+  def new_uninstall(mock: true)
+    new 'uninstall', mock
   end
 
   it 'instantiates with correct command' do
     command = Cman::Executor::COMMANDS[0]
-    expect { new command }.not_to raise_error
+    expect { new command, false }.not_to raise_error
   end
 
   it 'raise error on incorrect command' do
-    expect { new 'status123123' }.to raise_error
+    expect { new 'status123123', false }.to raise_error
   end
 
   it 'raise error on wrong number of arguments' do
-    comm = new 'add'
-    expect { comm.execute }.to raise_error
+    expect { new_add(false).execute }.to raise_error
   end
 
   it 'can add repository' do
-    comm = new 'add'
-
     repo_name = 'i3'
-
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
     path = File.join(BASE_DIR, repo_name)
     Dir.exist?(path).should be_true
   end
 
   it 'cannot add repository second time' do
-    comm = new 'add'
-
     repo_name = 'i3'
 
-    comm.should_receive(:gets).and_return('y', 'y')
-
-    comm.execute repo_name
-    expect { comm.execute repo_name }.to raise_error
+    new_add.execute repo_name
+    expect { new_add.execute repo_name }.to raise_error
   end
 
   it 'can add files to the repository' do
     repo_name = 'i3'
 
-    comm = new 'add'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
     file1 = '/test/file1'
     file2 = '/test/file2'
     file3 = '/test/file3'
     touch file1, file3
 
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name, file1, file2, file3, file1
+    new_add.execute repo_name, file1, file2, file3, file1
 
     path = Pathname(File.join(BASE_DIR, repo_name))
 
@@ -75,21 +85,16 @@ describe Cman::Executor do
     file2 = '/test/file2'
     touch file1, file2
 
-    comm = new 'add'
-    expect { comm.execute repo_name, file1, file2 }
+    expect { new_add(false).execute repo_name, file1, file2 }
       .to raise_error
   end
 
   it 'can remove repository' do
     repo_name = 'i3'
 
-    comm = new 'add'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
-    comm = new 'remove'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_remove.execute repo_name
 
     path = File.join(BASE_DIR, repo_name)
     Dir.exist?(path).should be_false
@@ -98,28 +103,21 @@ describe Cman::Executor do
   it 'cannot remove repository if it does not exist' do
     repo_name = 'i3'
 
-    comm = new 'remove'
-    comm.should_receive(:gets).and_return('y')
-    expect { comm.execute repo_name }.to raise_error
+    expect { new_remove.execute repo_name }.to raise_error
   end
 
   it 'can remove repository files' do
     repo_name = 'i3'
 
-    comm = new 'add'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
     file1 = '/test/file1'
     file2 = '/test/file2'
     touch file1, file2
 
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name, file1, file2
+    new_add.execute repo_name, file1, file2
 
-    comm = new 'remove'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name, '1', 'test' # 1 is repo file id
+    new_remove.execute repo_name, '1', 'test' # 1 is repo file id
 
     path = Pathname(File.join(BASE_DIR, repo_name))
 
@@ -128,40 +126,31 @@ describe Cman::Executor do
 
   it 'can show stats' do
     repo_name = 'i3'
-    comm = new 'add'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
-    comm = new 'stats'
-    comm.execute
+    new_stats.execute
 
-    comm.execute repo_name
+    new_stats.execute repo_name
   end
 
   it 'show stats for non existing repo' do
     repo_name = 'i3'
 
-    comm = new 'stats'
-    comm.execute repo_name
+    new_stats.execute repo_name
   end
 
   it 'can install files' do
     repo_name = 'i3'
 
-    comm = new 'add'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
     file1 = '/test/file1'
     file2 = '/test/file2'
     touch file1, file2
 
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name, file1, file2
+    new_add.execute repo_name, file1, file2
 
-    comm = new 'install'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name, '1'
+    new_install.execute repo_name, '1'
 
     File.symlink?(file2).should be_true
   end
@@ -169,20 +158,15 @@ describe Cman::Executor do
   it 'can install repository' do
     repo_name = 'i3'
 
-    comm = new 'add'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_add.execute repo_name
 
     file1 = '/test/file1'
     file2 = '/test/file2'
     touch file1, file2
 
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name, file1, file2
+    new_add.execute repo_name, file1, file2
 
-    comm = new 'install'
-    comm.should_receive(:gets).and_return('y')
-    comm.execute repo_name
+    new_install.execute repo_name
 
     File.symlink?(file1).should be_true
     File.symlink?(file2).should be_true
@@ -191,7 +175,22 @@ describe Cman::Executor do
   it 'cannot install files from unknown repo' do
     repo_name = 'i3'
 
-    comm = new 'install'
-    expect { comm.execute repo_name, '1' }.to raise_error
+    expect { new_install(false).execute repo_name, '1' }.to raise_error
+  end
+
+  it 'can uninstall files' do
+    repo_name = 'i3'
+
+    new_add.execute repo_name
+
+    file1 = '/test/file1'
+    file2 = '/test/file2'
+    touch file1, file2
+
+    new_add.execute repo_name, file1, file2
+
+    new_install.execute repo_name, '1'
+
+    File.symlink?(file2).should be_true
   end
 end
